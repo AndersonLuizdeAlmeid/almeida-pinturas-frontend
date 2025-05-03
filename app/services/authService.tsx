2c4ef885baa331bf426cc906825e41fe2af06503
Anderson Luiz de Almeida <anderson-luiza@hotmail.com>
2025-05-03 13:23:32 -0300

    Altered environment variables

--- app/services/authService.tsx
+++ app/services/authService.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const login = async (email: string, password: string) => {
   try {
--- hooks/useCreateUser.tsx
+++ hooks/useCreateUser.tsx
@@ -4,7 +4,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useState } from "react";
 import { toast } from "sonner";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const TOKEN_KEY = "token";
 
 export const useCreateUser = () => {
